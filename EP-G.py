10ca50b6e31bdb1308e62c5e7bbea44d209a8b38
MOJAAD <mojaad@outlook.com>
2020-08-18 22:14:27 +0430

    creat class

--- EP-G.py
+++ EP-G.py
@@ -14,9 +14,30 @@ from time import sleep
 from sys import exit,argv
 import sqlite3
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap,QFont
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QPixmap,QFont,QMovie,QPainter
 from PIL import Image
 #####################################################################################################################
+class window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(300,100,750,500)
+        self.setWindowTitle("ESTATE App")
+        self.UI()
+        self.show()
+
+    def UI(self):
+        pass
 
 
+
+#####################################################################################################################
+def main():
+    App = QApplication(argv)
+    Window = window()
+    exit(App.exec_())
+
+if __name__ == '__main__':
+    main()
+#####################################################################################################################
 
