d6c8e604d42a126a0852036db94a9d424b923699
MOJAAD <mojaad@outlook.com>
2020-08-18 21:58:11 +0430

    modules added

---
+++ EP-G.py
@@ -0,0 +1,22 @@
+#####################################################################################################################
+#                                                                                                                   #
+#                                                                                                                   #
+#                                             IN THE NAME OG GOD                                                    #
+#                                            PROJECT   OF  ESTATE                                                   #
+#                                           CRAETED BY :    MOJAAD                                                  #
+#                                             IN DATE: 2020/29/5                                                    #
+#                                                                                                                   #
+#                                                                                                                   #
+#################################################### MODULES ########################################################
+import pickle
+from os import path,system,name
+from time import sleep 
+from sys import exit,argv
+import sqlite3
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap,QFont
+from PIL import Image
+#####################################################################################################################
+
+
+
